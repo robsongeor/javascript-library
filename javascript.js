3d427eafc0476e42925a225827afa4d03ccb3849
robsongeor <37199574+robsongeor@users.noreply.github.com>
2025-04-18 22:02:32 +1200

    efactor code: mvoe book dom creating to seperate function
    WR:
    WR

--- javascript.js
+++ javascript.js
@@ -4,40 +4,47 @@ const bookContainer = document.querySelector(".book-container");
 
 const container = document.querySelector(".container");
 
+function createBookDOMElement(book){
+    let bookContainer = document.createElement("div");
+    bookContainer.classList.add("book-container")
+    
+    let titleElement = document.createElement("p")
+    titleElement.classList.add("book-title")
+    titleElement.textContent = book.title;
+    bookContainer.appendChild(titleElement);
+
+    let authorElement = document.createElement("p")
+    authorElement.classList.add("book-author")
+    authorElement.textContent = book.author;
+    bookContainer.appendChild(authorElement);
+    
+    let idElement = document.createElement("p")
+    idElement.classList.add("book-id")
+    idElement.textContent = book.id;
+    bookContainer.appendChild(idElement);
+
+    return bookContainer;
+}
+
 function Book(title, author, id){
     this.title = title;
     this.author = author;
     this.id = id;
+    this.domElement = createBookDOMElement(this)
 
-    this.bookContainer = document.createElement("div");
-    this.bookContainer.classList.add("book-container")
-
-    this.titleElement = document.createElement("p")
-    this.titleElement.classList.add("book-title")
-    this.titleElement.textContent = this.title;
-    this.bookContainer.appendChild(this.titleElement);
-
-    this.authorElement = document.createElement("p")
-    this.authorElement.classList.add("book-author")
-    this.authorElement.textContent = this.author;
-    this.bookContainer.appendChild(this.authorElement);
-    
-    this.idElement = document.createElement("p")
-    this.idElement.classList.add("book-id")
-    this.idElement.textContent = this.id;
-    this.bookContainer.appendChild(this.idElement);
-
-    container.appendChild(this.bookContainer);
+    container.appendChild(this.domElement);
 }
-
-
 
 function addBookToLibrary(title, author){
     myLibrary.push(new Book(title, author, crypto.randomUUID()));
 
 }
 
-addBookToLibrary("Harry Potter and The Chamber of Secrets", "david mckensies")
+for (let i = 0; i < 19; i++) {
+    addBookToLibrary("The Book of Books!", "david mckensies")
+    
+}
+
 console.table(myLibrary);
 
 
